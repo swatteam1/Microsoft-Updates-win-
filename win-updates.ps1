a16a969a8e5b9fffb278d7dab1b7dd5be0146b13
EugeneBruev <34741766+swatteam1@users.noreply.github.com>
2021-05-14 13:52:39 +0500

    Update win-updates.ps1

--- win-updates.ps1
+++ win-updates.ps1
@@ -125,7 +125,7 @@ function Install-WindowsUpdates() {
         LogWrite 'No updates available to install...'
         $global:MoreUpdates=0
         $global:RestartRequired=0
-        Invoke-Expression "a:\openssh.ps1 -AutoStart"
+        Invoke-Expression "С:\openssh.ps1 -AutoStart"
         break
     }
 
